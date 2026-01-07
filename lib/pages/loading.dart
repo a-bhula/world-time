2f8ad7085e10803b736560aee45205c5ea13922d
a-bhula <ammaarbhula76@gmail.com>
2026-01-07 17:51:03 +0000

    feat: add UTC time API with performance logging
    - Fetch UTC time from worldclockapi.com
    - Add API timing and error handling
    - Navigate to home after data load

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,43 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    print('Starting API call...');
+    final startTime = DateTime.now();
+    
+    try {
+      print('Making HTTP request...');
+      Response response = await get(Uri.parse('http://worldclockapi.com/api/json/utc/now'));
+      final duration = DateTime.now().difference(startTime);
+      
+      print('Response status: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        print('API loaded successfully in ${duration.inMilliseconds}ms');
+        Map data = jsonDecode(response.body);
+        print('Received time: ${data['currentDateTime']}');
+        print('UTC Offset: ${data['utcOffset']}');
+      } else {
+        print('API returned status ${response.statusCode} after ${duration.inMilliseconds}ms');
+      }
+    } catch (e) {
+      final duration = DateTime.now().difference(startTime);
+      print('API failed after ${duration.inMilliseconds}ms - Error: $e');
+    }
+    
+    // Navigate to home page
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('Loading page initialized');
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,3 +56,5 @@ class _LoadingState extends State<Loading> {
     );
   }
 }
+
+ 
